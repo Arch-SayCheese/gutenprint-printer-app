ab0dbd4dcce4c258855a0867f83359cd9950914e
Chris Desjardins <chris@arch-embedded.com>
2025-08-19 12:51:12 +0200

    Get build working for android

---
+++ gutenprint-printer-app.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+export FILTER_DIR=/system/bin/cups/filter
+export STATE_DIR=/data/gutenprint-printer-app
+export STATE_FILE=/data/gutenprint-printer-app/gutenprint-printer-app.state
+export PPD_DIRS=/system/etc/ppd:/system/bin/ppd
+export CUPS_DEBUG_LOG=-
+export CUPS_DEBUG_LEVEL=1
+#export STP_DEBUG=0xff7ff7ff
+export LD_LIBRARY_PATH=/system/lib64/printers
+export BACKEND_DIR=/system/bin/backends
+export HOME=/data
+export SPOOL_DIR=/data/spool/
+export CUPS_STATEDIR=/data/cups/run
+export CUPS_DATADIR=/system/etc/cups
+export CUPS_SERVERROOT=/data/cups/serverroot
+export TMPDIR=/data/local/tmp
+export TESTPAGE_DIR=/system/etc
+
+mkdir -p $STATE_DIR $CUPS_STATEDIR
+
+gutenprint-printer-app $@
