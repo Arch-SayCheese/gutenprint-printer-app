0a0bd074cfc0000b250f0046a2214f212a1ff600
Chris Desjardins <chris@arch-embedded.com>
2026-02-09 09:43:56 +0100

    Fix redirect issue

--- gutenprint-printer-app.sh
+++ gutenprint-printer-app.sh
@@ -16,6 +16,6 @@ export CUPS_SERVERROOT=/data/cups/serverroot
 export TMPDIR=/data/local/tmp
 export TESTPAGE_DIR=/system/etc
 
-mkdir -p $STATE_DIR $CUPS_STATEDIR 2>&1 /dev/null
+mkdir -p $STATE_DIR $CUPS_STATEDIR > /dev/null 2>&1
 
 gutenprint-printer-app $@
